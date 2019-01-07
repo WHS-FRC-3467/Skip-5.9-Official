bb69c52c531cb6eacdf0c51c1488a88351db5e19
adamf59 <af.frank.2001@gmail.com>
2019-01-07 18:39:05 -0500

    added core robot files

---
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.drivetrain;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drivetrain extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+}
---
+++ src/main/java/frc/robot/subsystems/pneumatics/Pneumatics.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystems.pneumatics;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Pneumatics extends Subsystem {
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+} 
