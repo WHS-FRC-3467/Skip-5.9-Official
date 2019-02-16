35107276f5933983d8250242e192e7c0d58b78a5
Scott Kukshtel <skukshtel@symbotic.com>
2019-02-15 21:12:31 -0500

    Removed frc.robot directory

--- src/main/java/frc/Subsystems/Hatch/DeployGrabber.java
+++
@@ -1,1 +0,0 @@
-//TODO write code for hatch grabber deployer
--- src/main/java/frc/Subsystems/LIDAR/ZeroLidar.java
+++
@@ -1,8 +0,0 @@
-package frc.Subsystems.LIDAR;
-
-import edu.wpi.first.wpilibj.command.InstantCommand;
-
-public class ZeroLidar extends InstantCommand {
-
-    //TODO zerolidar implementation
-}
